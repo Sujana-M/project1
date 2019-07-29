8bec265c404f44c9e8d66cd3fd2cb5d61b3f95c9
Sujana-M <sujana.m1526@gmail.com>
2019-07-29 11:29:19 +0530

    new

---
+++ calcul.c
@@ -0,0 +1,33 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "division.h"
+#include "add.h"
+
+void main()
+{
+    char ch;
+    int a, b;
+    printf("Enter the the operation +,-,*,/");
+    scanf("%c", &ch);
+    
+    printf("Enter the 2 numbers");
+    scanf("%d,%d", &a, &b);
+    
+    switch(ch)
+    {
+            case'+':
+                    add(a,b);
+                    break;
+            case '-':
+                    printf("the ans is %d", a-b);
+                    break;
+            case '*':
+                    printf("the ans is %d", a*b);
+                    break;
+            case '/':
+                    divi(a,b);
+                    break;
+           default:
+                   printf("Error in operation");
+    }
+}
---
+++ division.h
@@ -0,0 +1,1 @@
+void divi(int a,int b);
