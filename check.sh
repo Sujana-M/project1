e0aac891a32bf2e1e4bca5c69008c5fb9061b9de
Sujana M <sujana.m@arm.com>
2023-07-31 22:10:36 +0530

    Enable CC check 1

--- check.sh
+++ check.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/csh
 
+echo $cwd
+touch diff.txt
+touch modified.txt
+touch copyright.txt
+touch sdl_report.txt
 git diff --name-only HEAD~1 > diff.txt
 sed '/linux_app/d' diff.txt >  modified.txt
 sed -n '/.*\.h$/p;/.*\.c$/p;/.*\.S$/p;/.*\.py$/p;/.*\.mk$/p;' modified.txt > copyright.txt
-
+echo $cwd
+ls
 foreach line ( `cat copyright.txt`)
     ./copyright_checker.py $cwd/$line >> sdl_report.txt
 end
