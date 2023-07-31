b0fb4597a74caed92c770b985525e5c1f2950fba
Sujana M <sujana.m@arm.com>
2023-07-31 22:16:39 +0530

    prints1

--- check.sh
+++ check.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/sh
 
-echo $cwd
+echo $pwd
 touch diff.txt
 touch modified.txt
 touch copyright.txt
 touch sdl_report.txt
-git diff --name-only HEAD~1 > diff.txt
+#git diff --name-only HEAD~1 > diff.txt
+find . > diff.txt
 sed '/linux_app/d' diff.txt >  modified.txt
 sed -n '/.*\.h$/p;/.*\.c$/p;/.*\.S$/p;/.*\.py$/p;/.*\.mk$/p;' modified.txt > copyright.txt
-echo $cwd
+echo $pwd
 ls
 foreach line ( `cat copyright.txt`)
     ./copyright_checker.py $cwd/$line >> sdl_report.txt
