7411c226735c149beea72060e8ecac858146d2e8
Sujana M <sujana.m@arm.com>
2023-07-31 23:07:37 +0530

    prints3

--- check.sh
+++ check.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/sh
 
-echo $pwd
+lsb_release -a
+python --version
+python2 --version
+python3 --version
+echo $PWD
 touch diff.txt
 touch modified.txt
 touch copyright.txt
 touch sdl_report.txt
 #git diff --name-only HEAD~1 > diff.txt
-find . > diff.txt
+find $PWD > diff.txt
 sed '/linux_app/d' diff.txt >  modified.txt
 sed -n '/.*\.h$/p;/.*\.c$/p;/.*\.S$/p;/.*\.py$/p;/.*\.mk$/p;' modified.txt > copyright.txt
-echo $pwd
+echo $PWD
 ls
 for i in $(cat copyright.txt)
 do
-    ./copyright_checker.py $cwd/$line >> sdl_report.txt
+    python2 copyright_checker.py $cwd/$line >> sdl_report.txt
 done
 cat sdl_report.txt
